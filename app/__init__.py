94d9bc88d3e357102e35b588a3aa11595410d791
Mateo Cabrera <ccabreraa5@est.ups.edu.ec>
2026-06-22 15:59:36 -0500

    Init y config

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+def create_app():
+    app= Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app,db)
+    #Blueprints
+    from app.blueprints.public import public_bp
+    from app.blueprints.auth import auth_bp
+    from app.blueprints.admin import admin_bp
+
+    app.register_blueprint(public_bp)
+    app.register_blueprint(auth_bp,url_prefix='/auth')
+    app.register_blueprint(admin_bp,url_prefix='/admin')
+    
+    return app
