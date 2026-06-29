55e507dc3bf38e6f8c838f15b74d64efca55991d
Mateo Cabrera <ccabreraa5@est.ups.edu.ec>
2026-06-29 14:47:48 -0500

    Agregar public dash home . html

--- app/blueprints/public/routes.py
+++ app/blueprints/public/routes.py
@@ -3,12 +3,12 @@ from . import public_bp
 
 @public_bp.route('/') 
 def home():
-    return render_template('auth/home.html')
+    return render_template('public/home.html')
 
 @public_bp.route('/tienda') 
 def tienda():
-    return render_template('auth/tienda.html')
+    return render_template('public/tienda.html')
 
 @public_bp.route('/contacto') 
 def contacto():
-    return render_template('auth/contacto.html')
+    return render_template('public/contacto.html')
